083778c7f723380f2ede041a381a31f759403800
Internships <84063946+vin-stack@users.noreply.github.com>
2023-04-17 08:33:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ def main():
 				    db_filename = st.text_input("DB Filename")
 				    create_db = st.button('Create Database')
 				    if create_db:
-						if db_filename.endswith('.db'):
+					if db_filename.endswith('.db'):
 					    	conn = create_connection(db_filename)
 					    	st.write(conn) # success message?
 						else: 
