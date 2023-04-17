8ab5a1749ec5c0d160d14d69160b0b98d41456b9
Internships <84063946+vin-stack@users.noreply.github.com>
2023-04-17 09:11:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -215,36 +215,9 @@ def main():
 
 				    
 
-						col1,col2 = st.beta_columns(2)
-
-						with col1:
-							with st.form(key='query_form'):
-								raw_code = st.text_area("SQL Code Here")
-								submit_code = st.form_submit_button("Execute")
-
-							# Table of Info
-
-							with st.beta_expander("Table Info"):
-								table_info = {'city':city,'country':country,'countrylanguage':countrylanguage}
-								st.json(table_info)
-
-						# Results Layouts
-						with col2:
-							if submit_code:
-								st.info("Query Submitted")
-								st.code(raw_code)
-
-								# Results 
-								query_results = sql_executor(raw_code)
-								with st.beta_expander("Results"):
-									st.write(query_results)
-
-								with st.beta_expander("Pretty Table"):
-									query_df = pd.DataFrame(query_results)
-									st.dataframe(query_df)
-
-					else:
-						st.warning("Incorrect Username/Password")
+				
+			else:
+				st.warning("Incorrect Username/Password")
 
 	else:
 		st.subheader("Create An Account")
