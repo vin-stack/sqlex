927de7bc38efc611cbe3843b4cdd06a0cddd9338
Internships <84063946+vin-stack@users.noreply.github.com>
2023-04-17 08:38:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -148,9 +148,9 @@ def main():
 
 				    db_filename = st.text_input("DB Filename")
 				    create_db = st.button('Create Database')
-                    if create_db:
-                        if db_filename.endswith('.db'):
-                            conn = create_connection(db_filename)
+				    if create_db:
+					if db_filename.endswith('.db'):
+					    conn = create_connection(db_filename)
 					    	st.write(conn) # success message?
 						else: 
 					    	st.write('DB filename must end with .db, please retry.')
